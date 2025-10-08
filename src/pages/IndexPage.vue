f5f21877cdd810e99d744da8c12dd172cd070688
Anneen011 <anneenjunplod@gmailcom>
2025-10-08 10:07:25 +0700

    Add Basic Form to IndexPage

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,43 +1,28 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
+  <q-page padding>
+    <form>
+      <q-input label="Your name" v-model="name" />
+      <q-input label="Your age" type="number" v-model="age" />
+      <q-toggle label="I accept terms" v-model="accepted" />
+      <q-btn label="Submit" @click.prevent="submitForm" />
+      <q-btn label="Reset" @click.prevent="resetForm" color="secondary" />
+    </form>
   </q-page>
 </template>
 
-<script setup lang="ts">
-import { ref } from 'vue';
-import type { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/ExampleComponent.vue';
+<script setup>
+import { ref } from 'vue'
+const name = ref('')
+const age = ref('')
+const accepted = ref(false)
 
-const todos = ref<Todo[]>([
-  {
-    id: 1,
-    content: 'ct1',
-  },
-  {
-    id: 2,
-    content: 'ct2',
-  },
-  {
-    id: 3,
-    content: 'ct3',
-  },
-  {
-    id: 4,
-    content: 'ct4',
-  },
-  {
-    id: 5,
-    content: 'ct5',
-  },
-]);
+const submitForm = () => {
+  console.log({ name: name.value, age: age.value, accepted: accepted.value })
+}
 
-const meta = ref<Meta>({
-  totalCount: 1200,
-});
+const resetForm = () => {
+  name.value = ''
+  age.value = ''
+  accepted.value = false
+}
 </script>
